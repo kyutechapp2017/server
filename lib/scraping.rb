230e4b28e24a4a5bbfccdebc5533eb8716216c8f
Kazuo <kazuo@planningdev.com>
2017-02-27 23:24:05 +0900

    #8 テキストのスクレイピング完成！

--- lib/scraping.rb
+++ lib/scraping.rb
@@ -2,9 +2,20 @@ require 'open-uri'
 require 'nokogiri'
 
 url = "https://db.jimu.kyutech.ac.jp/cgi-bin/cbdb/db.cgi?page=DBRecord&did=357&qid=all&vid=24&rid=661&Head=&hid=&sid=n&rev=0&ssid=1-2448-4121-g196"
+charset = nil
 
 (660..662).each do |num|
   regexp = /rid=\d{3}/
-  url_display = url.gsub(regexp, 'rid=num') # ここのnumの使い方を教えてください
-  p url_display
+  url_scraping = url.gsub(regexp, "rid=#{num}")
+  
+  html = open(url_scraping) do |f|
+    charset = f.charset
+    f.read
+  end
+
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+
+  doc.css('td[class*="record-value-"]' ).each do |tr|
+    p tr.inner_text
+  end
 end
