85a5a8581698418dd4ce85cca5cbbe359013e70a
Kazuo <kazuo@planningdev.com>
2017-05-09 20:05:19 +0900

    #7 バリデーションのミスを修正

--- app/models/iizuka_studying_abroad.rb
+++ app/models/iizuka_studying_abroad.rb
@@ -3,5 +3,5 @@ class IizukaStudyingAbroad < ApplicationRecord
   validates :attachment_1_url, allow_blank: true, format: /\A#{URI::regexp(%w(http https))}\z/
   validates :attachment_2_url, allow_blank: true, format: /\A#{URI::regexp(%w(http https))}\z/
   validates :attachment_3_url, allow_blank: true, format: /\A#{URI::regexp(%w(http https))}\z/
-  validates :attachment_4_url, allow_blank: true, ormat: /\A#{URI::regexp(%w(http https))}\z/
+  validates :attachment_4_url, allow_blank: true, format: /\A#{URI::regexp(%w(http https))}\z/
 end
