c3efd601e92ab017107eb3a546927c93704464bc
Kazuo <kazuo@planningdev.com>
2017-09-08 11:26:40 +0900

    APIの返り血をgenre_idで場合分け

--- app/controllers/api/v3/bullet_in_boards_controller.rb
+++ app/controllers/api/v3/bullet_in_boards_controller.rb
@@ -1,12 +1,32 @@
 class Api::V3::BulletInBoardsController < ApplicationController
   def index
-    @iizukanotices = IizukaNotice.all
-    @iizukalectures = IizukaLecture.all
-    @iizukacalls = IizukaCall.all
-    @iizukaintensivelectures = IizukaIntensiveLecture.all
-    @iizukastudyingabroads = IizukaStudyingAbroad.all
-    @iizukascholarships = IizukaScholarship.all
-    @iizukaothers = IizukaOther.all
-    @iizukahomepages = IizukaHomepage.all
+    genre_id = params[:genre_id].to_i
+    # ここでupdated_atをdatetime型に修正する
+    if genre_id == 0 then
+      @iizukanotices = IizukaNotice.all
+      @iizukalectures = IizukaLecture.all
+      @iizukacalls = IizukaCall.all
+      @iizukaintensivelectures = IizukaIntensiveLecture.all
+      @iizukastudyingabroads = IizukaStudyingAbroad.all
+      @iizukascholarships = IizukaScholarship.all
+      @iizukaothers = IizukaOther.all
+      @iizukahomepages = IizukaHomepage.all
+    elsif genre_id == 1 then
+      @iizukanotices = IizukaNotice.all
+    elsif genre_id ==2 then
+      @iizukalectures = IizukaLecture.all
+    elsif genre_id ==3 then
+      @iizukacalls = IizukaCall.all
+    elsif genre_id ==4 then
+      @iizukaintensivelectures = IizukaIntensiveLecture.all
+    elsif genre_id ==5 then
+      @iizukastudyingabroads = IizukaStudyingAbroad.all
+    elsif genre_id ==6 then
+      @iizukascholarships = IizukaScholarship.all
+    elsif genre_id ==7 then
+      @iizukaothers = IizukaOther.all
+    elsif genre_id ==8 then
+      @iizukahomepages = IizukaHomepage.all
+    end
   end
 end
--- app/controllers/api/v3/schedules_controller.rb
+++ app/controllers/api/v3/schedules_controller.rb
@@ -1,10 +1,17 @@
 class Api::V3::SchedulesController < ApplicationController
   # before_action :set_poster, only: [:show, :update, :destroy]
   def index
-    @subjects = Subject.all
-    @weekperiods = WeekPeriod.all
-    @intermediates = Intermediate.all
-
+    genre_id = params[:genre_id].to_i
+    # ここでupdated_atをdatetime型に修正する
+    if genre_id == 0 then
+      @subjects = Subject.all
+      @weekperiods = WeekPeriod.all
+      @intermediates = Intermediate.all
+    elsif genre_id == 1 then
+      @subjects = Subject.all
+      @weekperiods = WeekPeriod.all
+      @intermediates = Intermediate.all
+    end
     # 追記した部分
     render 'index', formats: 'json', handlers: 'jbuilder'
   end
