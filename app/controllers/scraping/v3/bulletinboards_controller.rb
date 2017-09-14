9b6693c03d4aa7287373ceb5ce4baf7702386f07
Kazuo <kazuo@planningdev.com>
2017-09-14 22:55:23 +0900

    なんだか掲示板controllerが上手くいかない

--- app/controllers/scraping/v3/bulletinboards_controller.rb
+++ app/controllers/scraping/v3/bulletinboards_controller.rb
@@ -33,7 +33,60 @@ class Scraping::V3::BulletinboardsController < ApplicationController
         end
         iizuka.save
       when 5 then
-        iizuka = IizukaNotece.new(url: get_data[0][0], title: get_data[0][1],  department: get_data[0][3], grade: get_data[0][4], content: get_data[0][5], note: get_data[0][6], date: get_data[0][7])
+        iizuka = IizukaCall.new(url: get_data[0][0], title: get_data[0][1],  department: get_data[0][3], grade: get_data[0][4], content: get_data[0][5], note: get_data[0][6], date: get_data[0][7])
+        iizuka.save
+      when 8 then
+        iizuka = IizukaScholarship.new(url: get_data[0][0], title: get_data[0][1],  note: get_data[0][3], date: get_data[0][4], content: get_data[0][5])
+        if get_data[0][6] != nil then
+          iizuka.attachment_1 = get_data[0][6]
+          iizuka.attachment_1_url = get_data[0][7]
+          if get_data[0][8] != nil then
+            iizuka.attachment_2 = get_data[0][8]
+            iizuka.attachment_2_url = get_data[0][9]
+          end
+        end
+        iizuka.save
+      when 9 then
+        iizuka = IizukaIntensiveLecture.new(url: get_data[0][0], title: get_data[0][1], date: get_data[0][2], content: get_data[0][3], note: get_data[0][4])
+        if get_data[0][5] != nil then
+          iizuka.attachment_1 = get_data[0][5]
+          iizuka.attachment_1_url = get_data[0][6]
+          if get_data[0][7] != nil then
+            iizuka.attachment_2 = get_data[0][7]
+            iizuka.attachment_2_url = get_data[0][8]
+            if get_data[0][9] != nil then
+              iizuka.attachment_3 = get_data[0][9]
+              iizuka.attachment_3_url = get_data[0][10]
+              if get_data[0][11] != nil then
+                iizuka.attachment_4 = get_data[0][11]
+                iizuka.attachment_4_url = get_data[0][12]
+                if get_data[0][13] != nil then
+                  iizuka.attachment_5 = get_data[0][13]
+                  iizuka.attachment_5_url = get_data[0][14]
+                end
+              end
+            end
+          end
+        end
+        iizuka.save
+      when 10 then
+        iizuka = IizukaStudyingAbroad.new(url: get_data[0][0], title: get_data[0][1], responsibility: get_data[0][2], content: get_data[0][3], date: get_data[0][4])
+        if get_data[0][5] != nil then
+          iizuka.attachment_1 = get_data[0][5]
+          iizuka.attachment_1_url = get_data[0][6]
+          if get_data[0][7] != nil then
+            iizuka.attachment_2 = get_data[0][7]
+            iizuka.attachment_2_url = get_data[0][8]
+            if get_data[0][9] != nil then
+              iizuka.attachment_3 = get_data[0][9]
+              iizuka.attachment_3_url = get_data[0][10]
+              if get_data[0][11] != nil then
+                iizuka.attachment_4 = get_data[0][11]
+                iizuka.attachment_4_url = get_data[0][12]
+              end
+            end
+          end
+        end
         iizuka.save
       end
       idset.rid = get_data[1]
