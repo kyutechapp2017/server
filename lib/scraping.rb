ca456899b93ef6cbe11ca7db0e96b48e45436a61
Kazuo <kazuo@planningdev.com>
2017-10-01 11:19:22 +0900

    if文のthenを削除

--- lib/scraping.rb
+++ lib/scraping.rb
@@ -53,14 +53,14 @@ module Scraping
 
         doc.css(place).each do |td|
           datas[count] = td.inner_text.gsub(/　|\r/,"")
-          if(datas[count] =~ date_find) then
+          if(datas[count] =~ date_find)
             date_material = datas[count].match(date_find)
             datas[count] = DateTime.parse("#{date_material[1]}-#{date_material[2]}-#{date_material[3]} ")
           end
 
-          if td.css('a').to_s != "" then
+          if td.css('a').to_s != ""
             temp = td.css('a').to_s.gsub(regexp, "").gsub("amp;", "")
-            if temp =~ url_find then
+            if temp =~ url_find
               count = count + 1
               datas[count] = url_head+temp
             end
@@ -77,7 +77,7 @@ module Scraping
 
       end
     rescue
-      if datas == [""] then
+      if datas == [""]
         p "finished"
 
       else
@@ -90,19 +90,19 @@ module Scraping
     case scraping_did
     when 357 then
       iizuka = IizukaNotice.new(url: get_data[0], title: get_data[1], date: get_data[2], period: get_data[3], place_or_before: get_data[4], content: get_data[5], department: get_data[6], grade: get_data[7])
-      if get_data[8] != nil then
+      if get_data[8] != nil
         iizuka.attachment_1 = get_data[8]
         iizuka.attachment_1_url = get_data[9]
-        if get_data[10] != nil then
+        if get_data[10] != nil
           iizuka.attachment_2 = get_data[10]
           iizuka.attachment_2_url = get_data[11]
-          if get_data[12] != nil then
+          if get_data[12] != nil
             iizuka.attachment_3 = get_data[12]
             iizuka.attachment_3_url = get_data[13]
-            if get_data[14] != nil then
+            if get_data[14] != nil
               iizuka.attachment_4 = get_data[14]
               iizuka.attachment_4_url = get_data[15]
-              if get_data[16] != nil then
+              if get_data[16] != nil
                 iizuka.attachment_5 = get_data[16]
                 iizuka.attachment_5_url = get_data[17]
               end
@@ -116,10 +116,10 @@ module Scraping
       iizuka.save
     when 367 then
       iizuka = IizukaScholarship.new(url: get_data[0], title: get_data[1],  note: get_data[3], date: get_data[4], content: get_data[5])
-      if get_data[6] != nil then
+      if get_data[6] != nil
         iizuka.attachment_1 = get_data[6]
         iizuka.attachment_1_url = get_data[7]
-        if get_data[8] != nil then
+        if get_data[8] != nil
           iizuka.attachment_2 = get_data[8]
           iizuka.attachment_2_url = get_data[9]
         end
@@ -127,19 +127,19 @@ module Scraping
       iizuka.save
     when 379 then
       iizuka = IizukaIntensiveLecture.new(url: get_data[0], title: get_data[1], date: get_data[2], content: get_data[3], note: get_data[4])
-      if get_data[5] != nil then
+      if get_data[5] != nil
         iizuka.attachment_1 = get_data[5]
         iizuka.attachment_1_url = get_data[6]
-        if get_data[7] != nil then
+        if get_data[7] != nil
           iizuka.attachment_2 = get_data[7]
           iizuka.attachment_2_url = get_data[8]
-          if get_data[9] != nil then
+          if get_data[9] != nil
             iizuka.attachment_3 = get_data[9]
             iizuka.attachment_3_url = get_data[10]
-            if get_data[11] != nil then
+            if get_data[11] != nil
               iizuka.attachment_4 = get_data[11]
               iizuka.attachment_4_url = get_data[12]
-              if get_data[13] != nil then
+              if get_data[13] != nil
                 iizuka.attachment_5 = get_data[13]
                 iizuka.attachment_5_url = get_data[14]
               end
@@ -150,16 +150,16 @@ module Scraping
       iizuka.save
     when 372 then
       iizuka = IizukaStudyingAbroad.new(url: get_data[0], title: get_data[1], responsibility: get_data[2], content: get_data[3], date: get_data[4])
-      if get_data[5] != nil then
+      if get_data[5] != nil
         iizuka.attachment_1 = get_data[5]
         iizuka.attachment_1_url = get_data[6]
-        if get_data[7] != nil then
+        if get_data[7] != nil
           iizuka.attachment_2 = get_data[7]
           iizuka.attachment_2_url = get_data[8]
-          if get_data[9] != nil then
+          if get_data[9] != nil
             iizuka.attachment_3 = get_data[9]
             iizuka.attachment_3_url = get_data[10]
-            if get_data[11] != nil then
+            if get_data[11] != nil
               iizuka.attachment_4 = get_data[11]
               iizuka.attachment_4_url = get_data[12]
             end
@@ -186,7 +186,7 @@ module Scraping
     driver.get(url)
 
     lecture_year = year.to_s + "年度"
-    if campus_id == 11 then
+    if campus_id == 11
       campus_name = "情報工学部"
     end
 
@@ -242,7 +242,7 @@ module Scraping
           count_3_2 = 0
           doc.css("td[class*=\"plan__\"]").each do |div|
             temp_3[count_3_2] = div.inner_text
-            if count_3_2 == 2 then
+            if count_3_2 == 2
               datas_3[count_3] = temp_3.join("　")
               count_3 = count_3 + 1
               count_3_2 = 0
@@ -264,10 +264,10 @@ module Scraping
           datas[1] = "https://edragon-syllabus.jimu.kyutech.ac.jp/guest/syllabuses/direct?faculty_code=#{campus_id}&year=#{year}&subject_code="
           datas[2] = year
           (0..11).each do |i|
-            if i == 1 then
+            if i == 1
               datas[i+3] = datas_1[i].to_i
               datas[1] = datas[1] + datas_1[i] + "&class_code="
-            elsif i == 7 then
+            elsif i == 7
               case datas_1[i]
               when "第1クォーター" then
                 datas[i+3] = 1
@@ -284,15 +284,15 @@ module Scraping
               else
                 datas[i+3] = 7
               end
-            elsif i == 8 then
+            elsif i == 8
               datas[i+3] = datas_1[i]
               datas[1] = datas[1] + datas_1[i]
-            elsif i == 9 then # 時間割
+            elsif i == 9  # 時間割
               temp = datas_1[i].split(",")
               array = [""]
               temp.each do |tmp|
                 temp_week = [""]
-                if tmp =~ /.曜\s\d限/ then
+                if tmp =~ /.曜\s\d限/
                   case tmp.match(/./)[0]
                   when "月" then
                     temp_week[0] = 1
@@ -334,7 +334,7 @@ module Scraping
               end
             elsif i == 10
               datas[i+2] = datas_1[i]
-            elsif i == 11 then
+            elsif i == 11
               datas[i+2] = DateTime.parse(datas_1[i].gsub!(/\s\(.\)/, ""))
             else
               datas[i+3] = datas_1[i]
@@ -366,7 +366,7 @@ module Scraping
           # DB格納
           gdata = datas
           # db_set_of_syllabus(datas, periods)
-          if Subject.find_by(url: gdata[1], updated: gdata[13]) == nil then
+          if Subject.find_by(url: gdata[1], updated: gdata[13]) == nil
 
             subjects = Subject.new(campus_id: gdata[0], url: gdata[1], year: gdata[2], name: gdata[3], code: gdata[4], teacher: gdata[5], department: gdata[6], classification: gdata[7], num_of_unit: gdata[8], grade: gdata[9], term: gdata[10], number: gdata[11], place: gdata[12], updated: gdata[13], outline: gdata[14], placement: gdata[15], item: gdata[16], procedure: gdata[17], goal: gdata[18], criteria: gdata[19], preparation: gdata[20], keyword: gdata[21], textbook: gdata[22], reference: gdata[23], note: gdata[24], email: gdata[25])
             binding.pry
@@ -392,7 +392,7 @@ module Scraping
         end
       rescue
         p "#{j}:#{i}番目でエラー"
-        if i ==  1 then
+        if i ==  1
           finish = true
         else
           j = j + 1
@@ -406,7 +406,7 @@ module Scraping
   end
 
   # def db_set_of_syllabus(gdata, periods)
-  #   if Subject.find_by(url: gdata[1], updated: gdata[13]) == nil then
+  #   if Subject.find_by(url: gdata[1], updated: gdata[13]) == nil
   #
   #     subject = Subject.new(campus_id: gdata[0], url: gdata[1], year: gdata[2], name: gdata[3], code: gdata[4], teacher: gdata[5], department: gdata[6], classification: gdata[7], num_of_unit: gdata[8], grade: gdata[9], term: gdata[10], number: gdata[11], place: gdata[12], updated: gdata[13], outline: gdata[14], placement: gdata[15], item: gdata[16], procedure: gdata[17], goal: gdata[18], criteria: gdata[19], preparation: gdata[20], keyword: gdata[21], textbook: gdata[22], reference: gdata[23], note: gdata[24], email: gdata[25])
   #     subject.save
